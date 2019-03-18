172e6a41cbccf20731713c31e8878302f74da9dc
brenthuang <guang11cheng@qq.com>
2019-03-18 23:02:07 +0800

    增加日志

--- src/demo_server/ws/common_logic/common_logic.cpp
+++ src/demo_server/ws/common_logic/common_logic.cpp
@@ -75,6 +75,7 @@ void CommonLogic::OnClientClosed(const ConnGUID* conn_guid)
 void CommonLogic::OnRecvClientData(const ConnGUID* conn_guid, const void* data, size_t len)
 {
     // echo, for test only
+    LOG_DEBUG((char*) data << ", len: " << len);
     logic_ctx_.scheduler->SendToClient(conn_guid, data, len);
 }
 
